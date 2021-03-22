7077212f0571d53a392afe9d2960d6a4787cbf38
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-22 17:01:28 +0900

    내가 했던 코드보다 성능이 우월한 코드들...

---
+++ Level1/완주하지 못한 선수/완주하지못한선수_5.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+class Solution {
+	public String solution(String[] participant, String[] completion) {
+
+		Map<String, Long> participantMap = Arrays.asList(participant).stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+		for (String name : completion) {
+
+			Long value = participantMap.get(name) - 1L;
+
+			if (value == 0L) {
+				participantMap.remove(name);
+			} else {
+				participantMap.put(name, value);
+			}
+		}
+
+		return participantMap.keySet().iterator().next();
+	}
+}
