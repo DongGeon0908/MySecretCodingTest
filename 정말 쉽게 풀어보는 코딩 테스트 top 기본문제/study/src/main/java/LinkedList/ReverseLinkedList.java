58b708ab2dbf2bb8eed18344cd70ef0a28e7a5c0
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-07-05 19:05:39 +0900

    Queue And Stack

--- 정말 쉽게 풀어보는 코딩 테스트 top 기본문제/study/src/main/java/LinkedList/ReverseLinkedList.java
+++ 정말 쉽게 풀어보는 코딩 테스트 top 기본문제/study/src/main/java/LinkedList/ReverseLinkedList.java
@@ -1,26 +1,26 @@
 package LinkedList;
 
-class ListNode1{
+class ListNode{
     int val;
-    ListNode1 next;
-    ListNode1(int x){
+    ListNode next;
+    ListNode(int x){
         this.val = x;
     }
 }
 
 public class ReverseLinkedList {
     public static void main(String[] args) {
-        ListNode1 l1 = new ListNode1(1);
-        l1.next = new ListNode1(2);
-        l1.next.next = new ListNode1(3);
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
 
         printNode(l1);
-        ListNode1 head = reverseList(l1);
+        ListNode head = reverseList(l1);
         printNode(head);
     }
-    public static ListNode1 reverseList(ListNode1 current) {
-        ListNode1 prev = null;
-        ListNode1 next = null;
+    public static ListNode reverseList(ListNode current) {
+        ListNode prev = null;
+        ListNode next = null;
         while(current !=null) {
             next = current.next;
             current.next = prev;
@@ -32,7 +32,7 @@ public class ReverseLinkedList {
 
 
 
-    public static void printNode(ListNode1 head) {
+    public static void printNode(ListNode head) {
         System.out.println("printNOde: ");
         while(head != null) {
             System.out.println(head.val);
