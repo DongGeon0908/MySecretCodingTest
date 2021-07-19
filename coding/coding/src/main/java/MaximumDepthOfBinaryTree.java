e522279eb4df7b6e42660aa13ab34b9884e3d91f
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-07-20 00:29:02 +0900

    Maximum Depth Of Binary Tree with Recursive

---
+++ coding/coding/src/main/java/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,35 @@
+import com.sun.source.tree.Tree;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int x) {
+        this.val = x;
+    }
+}
+
+public class MaximumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        TreeNode tree = new TreeNode(3);
+        tree.left = new TreeNode(1);
+        tree.right = new TreeNode(4);
+        tree.left.left = new TreeNode(5);
+        tree.left.right = new TreeNode(8);
+        tree.left.left.left = new TreeNode(7);
+        MaximumDepthOfBinaryTree a = new MaximumDepthOfBinaryTree();
+        System.out.println(a.maxDepth(tree));
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        System.out.println(root.val);
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+
+        return Math.max(leftMax, rightMax);
+    }
+
+}
