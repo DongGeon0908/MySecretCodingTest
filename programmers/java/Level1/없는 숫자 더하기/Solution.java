0978e8a7b525443e2273b3765e920eb49da2e0f1
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2021-09-16 11:28:21 +0900

    Create Solution.java

---
+++ programmers/java/Level1/없는 숫자 더하기/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] numbers) {
+    
+        int result = 45;
+        
+        for(int num : numbers){
+            result -= num;
+        }
+        
+        return result;
+    }
+}
